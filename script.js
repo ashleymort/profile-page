57afb48f3df34e5adc60add2225296124e824ddf
Ashley M <ashleymort@hotmail.co.uk>
2021-06-15 10:26:26 +0100

    added dropdown functionality to projects section and backgound image for heading

---
+++ script.js
@@ -0,0 +1,12 @@
+const headPicture1 = document.getElementById('headerPic1');
+const headPicture2 = document.getElementById('headerPic2');
+
+headPicture1.onclick('headerPic1'); {
+    headPicture1.style.display = hidden;
+}
+headPicture2.onclick('headerPic2'); {
+    headPicture2.style.display = hidden;
+}
+const headingPicture = function () {
+
+}
